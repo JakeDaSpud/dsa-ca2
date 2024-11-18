0e9109e8e144b5d6e86aa8232cb258886305ae2e
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2024-11-18 08:35:16 +0000

    :fire: replaced BSTNode.h and BinaryTree.h with the REAL, PROPER files this time I SWEARRRRRR

--- Proyecto/src/BinaryTree.h
+++ Proyecto/src/BinaryTree.h
@@ -7,26 +7,28 @@
 #include <iostream>
 
 template <class T>
-class BinaryTree {
+class BinaryTree
+{
 private:
-	void addItemToArray(BSTNode<T>* node, int &pos, int *arr);
-
+	void addItemToArray(BSTNode<T>* node, int &pos, T *arr);
 public:
 	BSTNode<T> *root;
 	BinaryTree();
-	void add(T item);
-	bool remove(T item);
+	BinaryTree(const BinaryTree<T> &other);
+	BinaryTree<T> operator=(const BinaryTree<T>& other);
+	void add(T& item);
+	bool remove(T& item);
 	void clear();
 	int count();
+	T& get(T& item);
 
-	T* toArray();
 	void printInOrder();
 	void printInOrder(BSTNode<T> *node);
 	void printPreOrder();
 	void printPreOrder(BSTNode<T> *node);
 	void printPostOrder();
 	void printPostOrder(BSTNode<T> *node);
-	
+	T* toArray();
 	~BinaryTree();
 };
 
@@ -37,26 +39,146 @@ BinaryTree<T>::BinaryTree()
 }
 
 template <class T>
-void BinaryTree<T>::add(T item)
+BinaryTree<T>::BinaryTree(const BinaryTree<T>& other)
 {
-	
+	root = nullptr;
+	if(other.root != nullptr)
+		root = new BSTNode<T>(*other.root);
+}
+
+template <class T>
+BinaryTree<T> BinaryTree<T>::operator=(const BinaryTree<T>& other)
+{
+	if (this == &other)
+		return *this;
+	if (other.root != nullptr)
+		root = new BSTNode<T>(*other.root);
+	else
+		root = nullptr;
+	return *this;
+
+}
+
+template <class T>
+void BinaryTree<T>::add(T& item)
+{
+	if (root == nullptr)
+	{
+		root = new BSTNode<T>(item);
+		root->setItem(item);
+	}
+	else
+	{
+		root->add(item);
+	}
 }
 
 template <class T>
 int BinaryTree<T>::count()
 {
-	return 0;
+	if (root == nullptr)
+		return 0;
+	return root->count();
 }
 
 template <class T>
-bool BinaryTree<T>::remove(T item)
+bool BinaryTree<T>::remove(T& item)
 {
-	return false;
+	BSTNode<T> *toBeRemoved = root;
+	BSTNode<T> *parent = nullptr;
+	bool found = false;
+
+	while (!found && toBeRemoved != nullptr)
+	{
+
+		if (toBeRemoved->getItem() == item)
+		{
+
+			found = true;
+		}
+		else
+		{
+			parent = toBeRemoved;
+			if (toBeRemoved->getItem() > item)
+			{
+				toBeRemoved = toBeRemoved->getLeft();
+			}
+			else
+			{
+				toBeRemoved = toBeRemoved->getRight();
+			}
+		}
+	}
+	if (!found)
+		return false;
+
+	if (toBeRemoved->getLeft() == nullptr || toBeRemoved->getRight() == nullptr)
+	{
+		BSTNode<T> *newChild;
+		if (toBeRemoved->getLeft() == nullptr)
+		{
+			newChild = toBeRemoved->getRight();
+		}
+		else
+		{
+			newChild = toBeRemoved->getLeft();
+		}
+		if (parent == nullptr)
+		{
+			root = newChild;
+		}
+		else if (parent->getLeft() == toBeRemoved)
+		{
+			parent->setLeft(newChild);
+		}
+		else
+		{
+			parent->setRight(newChild);
+		}
+		return true;
+	}
+
+	BSTNode<T> *smallestParent = toBeRemoved;
+	BSTNode<T> *smallest = toBeRemoved->getRight();
+	while (smallest->getLeft() != nullptr)
+	{
+		smallestParent = smallest;
+		smallest = smallest->getLeft();
+	}
+	toBeRemoved->setItem(smallest->getItem());
+	if (smallestParent == toBeRemoved)
+	{
+		smallestParent->setRight(smallest->getRight());
+	}
+	else
+	{
+		smallestParent->setLeft(smallest->getRight());
+	}
+
 }
 
-// Helper Function to put the binary tree into a sorted array;
 template <class T>
-void BinaryTree<T>::addItemToArray(BSTNode<T>* node, int &pos, int *arr)
+T& BinaryTree<T>::get(T& item)
+{
+	bool found = false;
+	BSTNode<T>* current = root;
+	while (!found)
+	{
+	
+		if (current == nullptr)
+			break;
+		if (current->getItem() == item)
+			return current->getItem();
+		else if (current->getItem() > item)
+			current = current->getLeft();
+		else
+			current = current->getRight();
+	}
+	throw std::logic_error("Item not found");
+}
+
+template <class T>
+void BinaryTree<T>::addItemToArray(BSTNode<T>* node, int &pos, T *arr)
 {
 	if (node != nullptr)
 	{
@@ -68,7 +190,6 @@ void BinaryTree<T>::addItemToArray(BSTNode<T>* node, int &pos, int *arr)
 
 }
 
-//This function is used to convert this binary tree into a sorted array.
 template <class T>
 T* BinaryTree<T>::toArray()
 {
@@ -88,10 +209,13 @@ void BinaryTree<T>::clear()
 template <class T>
 BinaryTree<T>::~BinaryTree()
 {
-	delete root;
+	if (root != nullptr)
+	{
+		delete root;
+		root = nullptr;
+	}
 }
 
-// Print the entire tree using in order traversal
 template<class T>
 void BinaryTree<T>::printInOrder()
 {
@@ -99,15 +223,12 @@ void BinaryTree<T>::printInOrder()
 	std::cout << std::endl;
 }
 
-// Print a subtree using in order traversal
 template<class T>
 void BinaryTree<T>::printInOrder(BSTNode<T> *node)
 {
 	
-	
 }
 
-// Print the entire tree using pre order traversal
 template<class T>
 void BinaryTree<T>::printPreOrder()
 {
@@ -115,7 +236,6 @@ void BinaryTree<T>::printPreOrder()
 	std::cout << std::endl;
 }
 
-// Print a subtree using post order traversal
 template<class T>
 void BinaryTree<T>::printPreOrder(BSTNode<T> *node)
 {
@@ -123,7 +243,6 @@ void BinaryTree<T>::printPreOrder(BSTNode<T> *node)
 	
 }
 
-// Print the entire tree using post order traversal
 template<class T>
 void BinaryTree<T>::printPostOrder()
 {
@@ -131,11 +250,10 @@ void BinaryTree<T>::printPostOrder()
 	std::cout << std::endl;
 }
 
-// Print a subtree using post order traversal
 template<class T>
 void BinaryTree<T>::printPostOrder(BSTNode<T> *node)
 {
-	
+
 }
 
 #endif
--- Proyecto/src/TreeMap.h
+++ Proyecto/src/TreeMap.h
@@ -37,7 +37,7 @@ void TreeMap<K, V>::clear() {
 /// @return Returns true if this map contains a mapping for the specified key.
 template <typename K, typename V>
 bool TreeMap<K, V>::containsKey(K key) {
-
+    return false;
 };
 
 /// @brief Get the value of the mapping with a specified key.
@@ -47,7 +47,7 @@ bool TreeMap<K, V>::containsKey(K key) {
 /// @return Returns the value to which the specified key is mapped, or null if this map contains no mapping for the key.
 template <typename K, typename V>
 V& TreeMap<K, V>::get(K key) {
-
+    return nullptr;
 };
 
 /// @brief Returns a Set view of the keys contained in this map.
@@ -56,7 +56,7 @@ V& TreeMap<K, V>::get(K key) {
 /// @return Returns a Set view of the keys contained in this map.
 template <typename K, typename V>
 BinaryTree<K> TreeMap<K, V>::keySet() {
-
+    return nullptr;
 };
 
 /// @brief Associates the specified value with the specified key in this map.
@@ -66,7 +66,7 @@ BinaryTree<K> TreeMap<K, V>::keySet() {
 /// @param value Specified Input Value
 template <typename K, typename V>
 void TreeMap<K, V>::put(K key, V value) {
-
+    
 };
 
 /// @brief Get the integer value amount of key-value mappings in the map.
@@ -75,7 +75,7 @@ void TreeMap<K, V>::put(K key, V value) {
 /// @return Returns the integer of key-value mappings in this map.
 template <typename K, typename V>
 int TreeMap<K, V>::size() {
-
+    return 0;
 };
 
 /// @brief Removes the item denoted by the given key.
@@ -85,7 +85,7 @@ int TreeMap<K, V>::size() {
 /// @return True if successful, false otherwise.
 template <typename K, typename V>
 bool TreeMap<K, V>::removeKey(K key) {
-
+    return false;
 };
 
 /// @brief Get the value of the mapping with a specified key.
@@ -95,7 +95,7 @@ bool TreeMap<K, V>::removeKey(K key) {
 /// @return Returns the value to which the specified key is mapped, or null if this map contains no mapping for the key.
 template <typename K, typename V>
 V& TreeMap<K, V>::operator[](K key) {
-
+    return nullptr;
 };
 
 #endif
