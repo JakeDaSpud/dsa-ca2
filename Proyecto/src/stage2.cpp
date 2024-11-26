92d1e5b9e07c03f973e6001c6dbd70e642b34f14
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2024-11-26 15:13:40 +0000

    :bulb: added comments to stage2.cpp for todo

---
+++ Proyecto/src/stage2.cpp
@@ -0,0 +1,24 @@
+#include "TreeMap.h"
+#include <string>
+
+int main(int argc, char const *argv[]);
+void read_file(const std::string &file_path, TreeMap<char, std::string> &treemap);
+void menu();
+
+int main(int argc, char const *argv[])
+{
+    
+    // Check CLI args for file location, if argc > 1 then skip asking for file
+
+    // Ask for file location or read first .txt file in a Proyecto/data/ directory
+
+    // Parse file: Add all items to TreeMap<KVPair<char FIRST LETTER OF WORD, std::string WORD>>
+
+    // do while Print Menu
+    // 1. List letters with words
+    // 2. View words for a certain letter
+    // 0. Exit
+    // Enter: 
+
+    return 0;
+}
