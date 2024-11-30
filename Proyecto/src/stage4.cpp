4e46370e20ec6084525e4fb33fcc020df6c619e1
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2024-11-30 13:03:34 +0000

    :construction: still working on menus, doing a lot of dynamic sizing for the future table layout i'll need to print

--- Proyecto/src/stage4.cpp
+++ Proyecto/src/stage4.cpp
@@ -2,18 +2,22 @@
 #include <string>
 #include <fstream>
 
+struct data;
+std::ostream& operator<<(std::ostream& os, const data d);
+
 int main(int argc, char const *argv[]);
+std::string toupper(std::string &s);
 bool valid_file_path(const std::string &file_path);
-bool read_file(std::string &file_path, TreeMap<char, std::string> &treemap);
-void menu(TreeMap<char, std::string> &treemap);
+bool read_file(std::string &file_path, TreeMap<std::string, BinaryTree<data>> &treemap);
+void menu(TreeMap<std::string, BinaryTree<data>> &treemap);
 
 // Global Variables
 std::string file_path;
 bool tried_arg_file_path = false;
-TreeMap<char, std::string> treemap;
-BinaryTree<char> treemapSet;
-struct data;
-std::ostream& operator<<(std::ostream& os, const data d);
+TreeMap<std::string, BinaryTree<data>> treemap;
+BinaryTree<std::string> treemapSet;
+std::string dataHeaders[5] = {"Name", "Platform", "Genre", "Publisher", "Year"};
+int dataLength[5] = {(int)dataHeaders[0].length(), (int)dataHeaders[1].length(), (int)dataHeaders[2].length(), (int)dataHeaders[3].length(), (int)dataHeaders[4].length()};
 
 // These are going to be stored in a KVPair of type <std::string (name / primary key), data (other info)>
 // Struct for Data Rows
@@ -24,10 +28,18 @@ struct data {
     std::string publisher;
     int year;
 };
+typedef struct data data; // Here so I don't have to write stuct data everywhere, I can just write data
 
 std::ostream& operator<<(std::ostream& os, const data d) { 
     os << d.platform << ", "  << d.genre << ", "  << d.publisher << ", " << d.year << '\n';
     return os;
+}
+
+std::string toupper(std::string &s) {
+    for (int i = 0; i < s.length(); i++) {
+        s[i] = toupper(s[i]);
+    }
+    return s;
 }
 
 bool valid_file_path(const std::string &file_path) {
@@ -69,4 +81,94 @@ int main(int argc, char const *argv[]) {
     menu(treemap);
 
     return 0;
+}
+
+bool read_file(std::string &file_path, TreeMap<std::string, BinaryTree<data>> &treemap) {
+    std::ifstream file(file_path);
+
+    if (!file.is_open()) {
+        std::cerr << "Error: Couldn't open the file at " << file_path << '\n';
+        return false;
+    }
+
+    std::cout << "Success\n";
+
+
+
+    file.close();
+
+    // All words added, file parsed, make the keySet
+    treemapSet = treemap.keySet();
+
+    return true;
+}
+
+void menu(TreeMap<std::string, BinaryTree<data>> &treemap) {
+    std::string userInput;
+    char sentinel = '0';
+
+    do {
+        std::cout <<
+        "\nMenu\n" <<
+        "1. Index a data field (View: Name, Field of Choice)\n" <<
+        "2. View data with a specific value (In a specific Field of Choice)\n" <<
+        "0. Exit\n" <<
+        "Enter: ";
+        std::getline(std::cin, userInput);
+
+        // Make uppercase
+        userInput = toupper(userInput);
+
+        switch (userInput[0]) {
+        case '1':
+            std::cout << "\n[1] Choose one of the following fields to index for:\n" <<
+            "1. Name\n" <<
+            "2. Platform\n" <<
+            "3. Genre\n" <<
+            "4. Publisher\n" <<
+            "5. Year\n" <<
+            "Enter: ";
+            std::getline(std::cin, userInput);
+
+            switch (userInput[0]) {
+            case '1':
+                
+                break;
+
+            case '2': case '3': case '4': case '5':
+                break;
+            
+            default:
+                std::cout << "\nUnrecognised command!\n";
+                break;
+            }
+
+            break;
+
+        case '2':
+            std::cout << "\n[2] Choose one of the following letters to view the words for:\n" ;
+            // print treemapSet
+            treemapSet.printPreOrder();
+            std::cout << "Enter: ";
+            std::cin >> userInput;
+
+            userInput = toupper(userInput);
+
+
+
+            std::cout << '\n';
+            break;
+
+        case '0':
+            // End app, break loop
+            break;
+        
+        default:
+            std::cout << "\nUnrecognised command!\n";
+            break;
+        }
+
+    } while (userInput.length() != 1 && userInput[0] != sentinel);
+
+    std::cout << "\nGoodbye! :D\n";
 }
